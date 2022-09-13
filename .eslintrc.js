5b174a6bc5ea84a0e1cb7d511ce0641e06ae6494
testtible <gurwo100000@gmail.com>
2022-09-14 07:30:31 +0900

    settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,21 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
-    "eslint: recommended"
-  ]
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
 }
