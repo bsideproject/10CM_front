76e94d4998b32f8844058691b4cc19a9d509250c
herbert.kwon <herbert.kwon@honeynaps.com>
2022-09-14 11:41:56 +0900

    update

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,9 +2,9 @@ module.exports = {
   singleQuote: true,
   // 문자열은 singleQuote ("" -> '')
   semi: true,
-  // 코드 마지막에 세미콜론 있도록 formatting
-  tabWidth: 4,
-  // 들여쓰기는 4, 합의가 필요
+  // 코드 마지막에 세미콜론 있도록 formatting (합의 필요)
+  tabWidth: 2,
+  // 들여쓰기는 2, 합의가 필요
   trailingComma: 'all',
   // 배열 키:값 뒤에 항상 콤마 포맷팅
   arrowParens: 'avoid',
