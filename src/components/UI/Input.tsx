14690c846d19e9e637dc44c8bd865d99ea739cef
herbert.kwon <herbert.kwon@honeynaps.com>
2022-10-04 21:20:33 +0900

    update: input component

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,10 +1,26 @@
-import React, { ReactNode } from 'react';
-import styled from 'styled-components';
-interface IProps {}
+import React, {
+  ReactNode,
+  forwardRef,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  FC,
+} from 'react';
 
-const Input: React.FC<IProps> = () => {
-  return <InputContent />;
-};
+import styled from 'styled-components';
+
+export type InputType = 'text';
+interface IProps {
+  id: string;
+  name: string;
+  type?: InputType;
+  className?: string;
+}
+
+const Input: FC<IProps> = forwardRef<HTMLInputElement, IProps>(
+  ({ id, name, type = 'text', className = '' }, ref) => {
+    return <InputContent id={id} name={name} type={type} />;
+  },
+);
 
 const InputContent = styled.input``;
 
