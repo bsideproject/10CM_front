1b2ccc6cdbd68f97743f1bfbaf3c91ac0812a7f2
hyukjae <gurwo100000@gmail.com>
2022-10-10 15:37:51 +0900

    updating: map options, not complete

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,22 +3,22 @@ import styled from 'styled-components';
 import Button from './UI/Button';
 import Input from './UI/Input';
 
-const LoginForm = () => {
-  const [loginInfo, setLoginInfo] = useState({});
-  const handleSubmit = () => {};
+// const LoginForm = () => {
+//   const [loginInfo, setLoginInfo] = useState({});
+//   const handleSubmit = () => {};
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Input id="id" name="id" />
-      <Input id="pw" name="pw" />
-      <Button />
-    </Form>
-  );
-};
-// 작성 중 !!!!!!!!
-const Form = styled.form``;
+//   return (
+//     // <Form onSubmit={handleSubmit}>
+//     //   <Input id="id" name="id" />
+//     //   <Input id="pw" name="pw" />
+//     //   <Button />
+//     // </Form>
+//   );
+// };
+// // 작성 중 !!!!!!!!
+// const Form = styled.form``;
 
-export default LoginForm;
+// export default LoginForm;
 
 // context api 사용하여 전역적으로 정보 관리 예정
 
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode, useRef, useState, useEffect } from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import styled from 'styled-components';
 import { Address, LatLng } from 'types/dtos/address';
+import MapConfig from 'services/map-config.js';
 interface Props {}
 
 const Map: React.FC<Props> = () => {
@@ -9,73 +10,17 @@ const Map: React.FC<Props> = () => {
   let manager: any;
 
   const { kakao } = window;
-  console.log(window.kakao);
   const [test, setState] = useState<boolean>(false);
-
+  const kakaoMap = document.getElementById('map');
   useEffect(() => {
-    const testMap = document.getElementById('map');
     const options = {
       // 지도를 생성할 때 필요한 기본 옵션
       center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표.
       level: 3, // 지도의 레벨(확대, 축소 정도)
     };
-    map.current = new kakao.maps.Map(testMap, options); // 지도 생성 및 객체 리턴
+    map.current = new kakao.maps.Map(kakaoMap, options); // 지도 생성 및 객체 리턴
 
-    const managerOptions = {
-      // Drawing Manager를 생성할 때 사용할 옵션입니다
-      map: map.current, // Drawing Manager로 그리기 요소를 그릴 map 객체입니다
-      drawingMode: [
-        // drawing manager로 제공할 그리기 요소 모드입니다
-        kakao.maps.drawing.OverlayType.MARKER,
-        kakao.maps.drawing.OverlayType.POLYLINE,
-        kakao.maps.drawing.OverlayType.RECTANGLE,
-        kakao.maps.drawing.OverlayType.CIRCLE,
-        kakao.maps.drawing.OverlayType.POLYGON,
-      ],
-      // 사용자에게 제공할 그리기 가이드 툴팁입니다
-      // 사용자에게 도형을 그릴때, 드래그할때, 수정할때 가이드 툴팁을 표시하도록 설정합니다
-      guideTooltip: ['draw', 'drag', 'edit'],
-      markerOptions: {
-        // 마커 옵션입니다
-        draggable: true, // 마커를 그리고 나서 드래그 가능하게 합니다
-        removable: true, // 마커를 삭제 할 수 있도록 x 버튼이 표시됩니다
-      },
-      polylineOptions: {
-        // 선 옵션입니다
-        draggable: true, // 그린 후 드래그가 가능하도록 설정합니다
-        removable: true, // 그린 후 삭제 할 수 있도록 x 버튼이 표시됩니다
-        editable: true, // 그린 후 수정할 수 있도록 설정합니다
-        strokeColor: '#39f', // 선 색
-        hintStrokeStyle: 'dash', // 그리중 마우스를 따라다니는 보조선의 선 스타일
-        hintStrokeOpacity: 0.5, // 그리중 마우스를 따라다니는 보조선의 투명도
-      },
-      rectangleOptions: {
-        draggable: true,
-        removable: true,
-        editable: true,
-        strokeColor: '#39f', // 외곽선 색
-        fillColor: '#39f', // 채우기 색
-        fillOpacity: 0.5, // 채우기색 투명도
-      },
-      circleOptions: {
-        draggable: true,
-        removable: true,
-        editable: true,
-        strokeColor: '#39f',
-        fillColor: '#39f',
-        fillOpacity: 0.5,
-      },
-      polygonOptions: {
-        draggable: true,
-        removable: true,
-        editable: true,
-        strokeColor: '#39f',
-        fillColor: '#39f',
-        fillOpacity: 0.5,
-        hintStrokeStyle: 'dash',
-        hintStrokeOpacity: 0.5,
-      },
-    };
+    const managerOptions = MapConfig.managerOptions(kakao, map);
     manager = new kakao.maps.drawing.DrawingManager(managerOptions);
   }, []);
   // 지도 생성
@@ -100,65 +45,18 @@ const Map: React.FC<Props> = () => {
     }
   }, []);
   // 맵 이벤트 등록
-
   const createMarker = () => {
-    const markerPosition = new kakao.maps.LatLng(33.450711, 126.570611);
-
-    const marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(map.current);
+    // locationY, locationX 예시
+    MapConfig.createMarker(kakao, map, 32, 26);
   };
-  // 마커 생성
 
   const createCluster = () => {
-    const clusterer = new kakao.maps.MarkerClusterer({
-      map: map.current, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-      averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-      minLevel: 3, // 클러스터 할 최소 지도 레벨
-    });
-    const markers = [
-      new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(33.450711, 126.570611),
-      }),
-      new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(33.451021, 126.574833),
-      }),
-      new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(33.450231, 126.5739444),
-      }),
-      new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(33.451141, 126.575332),
-      }),
-    ];
-    clusterer.addMarkers(markers);
+    const locations = {};
+    MapConfig.createCluster(kakao, map, locations);
   };
-  // 클러스터 생성
+  // 마커 생성 : MapConfig.createMarker(kakao, map, locationY, locationX);
 
-  const displayMarker = (locPosition: any, message: string) => {
-    const marker = new kakao.maps.Marker({
-      map: map.current,
-      position: locPosition,
-    });
-    const infowindow = new kakao.maps.InfoWindow({
-      position: locPosition,
-      removable: true,
-      content: '<span class="info-title">말풍선타이틀</span>', // 인포윈도우 내부에 들어갈 컨텐츠 입니다.
-    });
-
-    infowindow.open(map.current, marker);
-    const infoTitle = document.querySelectorAll('.info-title');
-    infoTitle.forEach(el => {
-      const element = el;
-      if (element.parentElement?.parentElement) {
-        element.parentElement.parentElement.style.border = '0';
-        element.parentElement.parentElement.style.backgroundColor =
-          'transparent';
-      }
-    });
-    // 지도 중심좌표를 접속위치로 변경합니다
-    map.current.setCenter(locPosition);
-  };
+  // 클러스터 생성 : MapConfig.createCluster(kakao, map, -locations);
 
   const searchHome = () => {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -168,7 +66,7 @@ const Map: React.FC<Props> = () => {
       const locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
       const message = '<div>하이</div>'; // 인포윈도우에 표시될 내용입니다
       // 마커와 인포윈도우를 표시합니다
-      displayMarker(locPosition, message);
+      MapConfig.displayMarker(kakao, map, locPosition, message);
     });
   };
   // 내 위치에 마커 찍기
@@ -274,12 +172,11 @@ const Map: React.FC<Props> = () => {
     }).open();
   };
   const handleClear = () => {
-    const testMap = document.getElementById('map');
     const options = {
       center: map.current.getCenter(),
       level: map.current.getLevel(),
     };
-    map.current = new kakao.maps.Map(testMap, options); // 지도 생성 및 객체 리턴
+    map.current = new kakao.maps.Map(kakaoMap, options); // 지도 생성 및 객체 리턴
   };
 
   const handleCreateCursorMarker = (type: string) => {
