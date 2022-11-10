746b9a2038388285dccbc09b6ee912701038d4ca
KyeongS <leeks9653@naver.com>
2022-11-11 01:00:31 +0900

    feat: date-picker

---
+++ src/components/common/DatePicker/index.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import DatePicker from '.';
+
+export default {
+  title: 'Component/DatePicker',
+  component: DatePicker,
+} as ComponentMeta<typeof DatePicker>;
+
+const Template: ComponentStory<typeof DatePicker> = args => (
+  <DatePicker {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
