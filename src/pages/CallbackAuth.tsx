82f1d9a04a97042944d20fd96caed903899fd8d6
hyukjae <gurwo100000@gmail.com>
2023-05-27 14:58:07 +0900

    update: callback에서 로그인 중이라는 문구 추가

--- src/pages/CallbackAuth.tsx
+++ src/pages/CallbackAuth.tsx
@@ -4,7 +4,8 @@ import qs from 'qs';
 import { routePath } from 'constants/route';
 import { useAppDispatch, useAppSelect } from 'store/configureStore.hooks';
 import { asyncUserFetch } from 'store/modules/authInfo';
-import { shallowEqual } from 'react-redux';
+import styled from 'styled-components';
+import LoadingSpinner from 'components/common/LoadingSpinner';
 
 const CallbackAuth = () => {
   const location = useLocation();
@@ -32,7 +33,21 @@ const CallbackAuth = () => {
   }, [isLoggedIn]);
   // ref: navigate는 사용자 작동이나 훅에 의해 동작
 
-  return <div />;
+  return (
+    <Wrap>
+      <LoadingSpinner />
+      <span>로그인 중 입니다.</span>
+    </Wrap>
+  );
 };
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+`;
 
 export default CallbackAuth;
